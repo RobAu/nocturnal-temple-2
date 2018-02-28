be66e459a04854f3583bf3eb0d5fb6595f43ad3c
Rob Audenaerde <rob.audenaerde@gmail.com>
2018-02-28 16:47:28 +0100

    Added menu

--- nocturnal-temple/src/main/java/org/audenaerde/Main.java
+++ nocturnal-temple/src/main/java/org/audenaerde/Main.java
@@ -1,19 +1,16 @@
 package org.audenaerde;
 
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.io.FileNotFoundException;
-
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -22,23 +19,34 @@ import javafx.stage.Stage;
  *
  */
 public class Main extends Application {
-	
-	
+
 	/**
-	 * See: https://opengameart.org/content/rpg-tiles-cobble-stone-paths-town-objects
+	 * See:
+	 * https://opengameart.org/content/rpg-tiles-cobble-stone-paths-town-objects
 	 */
 	Image image = new Image(getClass().getResourceAsStream("/PathAndObjects.png"));
 
-	int mx =0;
-	int my =0;
-	
+	int mx = 0;
+	int my = 0;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Hello World!");
-		Group root = new Group();
-		Canvas canvas = new Canvas(400, 400);
 
-		root.getChildren().add(canvas);
+		BorderPane borderPane = new BorderPane();
+
+		// Put menu bar on the top of the window
+		MenuBar menuBar = new MenuBar(new Menu("File"), new Menu("Edit"), new Menu("Help"));
+		borderPane.setTop(menuBar);
+
+		// Put canvas in the center of the window (*)
+		Canvas canvas = new Canvas(400, 400);
+		borderPane.setCenter(canvas);
+		// Bind the width/height property so that the size of the Canvas will be
+		// resized as the window is resized
+		canvas.widthProperty().bind(borderPane.widthProperty());
+		canvas.heightProperty().bind(borderPane.heightProperty());
+
+		primaryStage.setTitle("Hello World!");
 
 		AnimationTimer timer = new AnimationTimer() {
 
@@ -48,14 +56,14 @@ public class Main extends Application {
 				drawShapes(gc);
 			}
 		};
-		Scene s = new Scene(root, 400, 400);
+		Scene s = new Scene(borderPane, 850, 450);
 
 		EventHandler<MouseEvent> movedHandler = new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent arg0) {
 				mx = (int) arg0.getSceneX();
-				my= (int) arg0.getSceneY();
+				my = (int) arg0.getSceneY();
 			}
 		};
 		s.setOnMouseMoved(movedHandler);
@@ -67,15 +75,14 @@ public class Main extends Application {
 
 	private void drawShapes(GraphicsContext gc) {
 
-		final Point p = MouseInfo.getPointerInfo().getLocation();
-
+		gc.clearRect(0, 0, 400, 400);
 		gc.drawImage(image, 0, 0);
 		gc.setStroke(Color.BLUE);
-		gc.setLineWidth(1);
-		
+		gc.setLineWidth(2);
+
 		int tileSize = 32;
-		int tx = (mx/tileSize)*tileSize;
-		int ty = (my/tileSize)*tileSize;
+		int tx = (mx / tileSize) * tileSize;
+		int ty = (my / tileSize) * tileSize;
 
 		gc.strokeRect(tx, ty, tileSize, tileSize);
 

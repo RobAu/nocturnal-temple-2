9351ffa5e0bd8585c9f3a972a18ca71deaca089e
Rob Audenaerde <rob.audenaerde@gmail.com>
2018-03-02 09:38:58 +0100

    movement.

--- nocturnal-temple/src/main/java/org/audenaerde/Main.java
+++ nocturnal-temple/src/main/java/org/audenaerde/Main.java
@@ -1,6 +1,5 @@
 package org.audenaerde;
 
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -8,7 +7,9 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -27,69 +28,122 @@ public class Main extends Application {
 	 */
 	Image image = new Image(getClass().getResourceAsStream("/PathAndObjects.png"));
 
+	int tileSize = 32;
+
 	int mx = 0;
 	int my = 0;
+	int tx = (mx / tileSize);
+	int ty = (my / tileSize);
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		BorderPane borderPane = new BorderPane();
-		 Pane wrapperPane = new Pane();
-		    borderPane.setCenter(wrapperPane);
-		    // Put canvas in the center of the window
-		
+		Pane wrapperPane = new Pane();
+		borderPane.setCenter(wrapperPane);
+		// Put canvas in the center of the window
 
 		// Put menu bar on the top of the window
-		MenuBar menuBar = new MenuBar(new Menu("File"), new Menu("Edit"), new Menu("Help"));
+		Menu fileMenu = new Menu("File");
+		MenuItem saveItem = new MenuItem("Save");
+		saveItem.setOnAction(e -> System.out.println(e));
+		fileMenu.getItems().addAll(saveItem);
+
+		MenuBar menuBar = new MenuBar(fileMenu, new Menu("Edit"), new Menu("Help"));
 		borderPane.setTop(menuBar);
 
 		// Put canvas in the center of the window (*)
-		Canvas canvas = new Canvas(400, 400);
-		   wrapperPane.getChildren().add(canvas);
+		Canvas canvas1 = new Canvas(400, 400);
+		wrapperPane.getChildren().add(canvas1);
 		// Bind the width/height property so that the size of the Canvas will be
 		// resized as the window is resized
-		canvas.widthProperty().bind(borderPane.widthProperty());
-		canvas.heightProperty().bind(borderPane.heightProperty());
+		canvas1.widthProperty().bind(borderPane.widthProperty());
+		canvas1.heightProperty().bind(borderPane.heightProperty());
 
 		primaryStage.setTitle("Hello World!");
 
-		AnimationTimer timer = new AnimationTimer() {
-
-			@Override
-			public void handle(long arg0) {
-				GraphicsContext gc = canvas.getGraphicsContext2D();
-				drawShapes(gc, canvas.getWidth(), canvas.getHeight());
-			}
-		};
+		// AnimationTimer timer = new AnimationTimer() {
+		//
+		// @Override
+		// public void handle(long arg0) {
+		//
+		// }
+		// };
 		Scene s = new Scene(borderPane, 850, 450);
+
+		s.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				switch (event.getCode()) {
+				case DIGIT1:
+					if (event.isControlDown())
+						System.out.println("Set 1 to tile");
+					;
+					break;
+
+				case UP:
+					ty--;
+					break;
+
+				case LEFT:
+					tx--;
+					break;
+
+				case RIGHT:
+					tx++;
+					break;
+
+				case DOWN:
+					ty++;
+					break;
+				}
+
+				GraphicsContext gc = canvas1.getGraphicsContext2D();
+
+				drawShapes(gc, canvas1.getWidth(), canvas1.getHeight());
+			}
+		});
+
+		GraphicsContext gc = canvas1.getGraphicsContext2D();
+
+		drawShapes(gc, canvas1.getWidth(), canvas1.getHeight());
 
 		EventHandler<MouseEvent> movedHandler = new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent arg0) {
+				GraphicsContext gc = canvas1.getGraphicsContext2D();
 				mx = (int) arg0.getX();
 				my = (int) arg0.getY();
+				tx = (mx / tileSize);
+				ty = (my / tileSize);
+				drawShapes(gc, canvas1.getWidth(), canvas1.getHeight());
+
 			}
 		};
-		canvas.setOnMouseMoved(movedHandler);
+		canvas1.setOnMouseMoved(movedHandler);
+		Object currentTile = null;
+		canvas1.setOnMousePressed(e -> setTile(currentTile));
 		primaryStage.setScene(s);
 		primaryStage.show();
 
-		timer.start();
+		// timer.start();
+	}
+
+	private Object setTile(Object currentTile) {
+		System.out.println("Setting tile");
+		return null;
 	}
 
 	private void drawShapes(GraphicsContext gc, double cw, double ch) {
 
 		gc.clearRect(0, 0, cw, ch);
 		gc.drawImage(image, 0, 0);
+		gc.drawImage(image, 32, 32, 32, 32, 400, 0, 32, 32);
 		gc.setStroke(Color.BLUE);
 		gc.setLineWidth(2);
 
-		int tileSize = 32;
-		int tx = (mx / tileSize) * tileSize;
-		int ty = (my / tileSize) * tileSize;
-
-		gc.strokeRect(tx, ty, tileSize, tileSize);
+		gc.strokeRect(tx * tileSize, ty * tileSize, tileSize, tileSize);
 
 	}
 

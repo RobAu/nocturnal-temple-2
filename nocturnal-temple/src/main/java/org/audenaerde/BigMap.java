48ce7ba275d6d6e4219442708a3cf6931f5c5b1a
Rob Audenaerde <rob.audenaerde@gmail.com>
2018-03-15 14:49:42 +0100

    Added object layer. Now see that the drawing order is wrong :o

--- nocturnal-temple/src/main/java/org/audenaerde/BigMap.java
+++ nocturnal-temple/src/main/java/org/audenaerde/BigMap.java
@@ -14,15 +14,15 @@ import tiled.io.TMXMapReader;
 public class BigMap {
 
 	int width;
-	int height ;
+	int height;
 	Map testMap;
 
-	java.util.Map<Integer, Image> tileCache= new HashMap<>();
-	
+	java.util.Map<Integer, Image> tileCache = new HashMap<>();
+
 	public BigMap() {
 		try {
 			testMap = new TMXMapReader().readMap("src/main/resources/test.tmx");
-			width = testMap.getWidth() ;
+			width = testMap.getWidth();
 			height = testMap.getHeight();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -34,20 +34,33 @@ public class BigMap {
 	public void drawTo(GraphicsContext gc) {
 		for (int y = 0; y < height; y++)
 			for (int x = 0; x < width; x++) {
-				TileLayer later = (TileLayer) testMap.getLayer(0);
-				Tile t = later.getTileAt(x, y);
-				
-				Image image = tileCache.get(t.getId());
-				if (image == null)
 				{
-					image = SwingFXUtils.toFXImage((BufferedImage) t.getImage(), null);
-					tileCache.put(t.getId(), image);
+					TileLayer layer = (TileLayer) testMap.getLayer(0);
+					Tile t = layer.getTileAt(x, y);
+					if (t != null) {
+						Image image = tileCache.get(10000+t.getId());
+						if (image == null) {
+							image = SwingFXUtils.toFXImage((BufferedImage) t.getImage(), null);
+							tileCache.put(10000+t.getId(), image);
+						}
+						gc.drawImage(image, 0, 0, t.getWidth(), t.getHeight(), x * t.getWidth(), y * t.getHeight(),
+								t.getWidth(), t.getHeight());
+					}
 				}
-				gc.drawImage(image, 0, 0, t.getWidth(), t.getHeight(), x*t.getWidth(), y*t.getHeight(), t.getWidth(),t.getHeight());
-
-				
+				{
+					TileLayer layer = (TileLayer) testMap.getLayer(1);
+					Tile t = layer.getTileAt(x, y);
+					if (t != null) {
+						Image image = tileCache.get(t.getId());
+						if (image == null) {
+							image = SwingFXUtils.toFXImage((BufferedImage) t.getImage(), null);
+							tileCache.put(t.getId(), image);
+						}
+						gc.drawImage(image, 0, 0, t.getWidth(), t.getHeight(), x * t.getWidth(), y * t.getHeight(),
+								t.getWidth(), t.getHeight());
+					}
+				}
 			}
 	}
 
-	
 }
